ab9990ba6b5dad2de8b2b222cdeca0f056418011
Zhenyu Lin <zhenyu.lyn@gmail.com>
2018-11-20 14:37:28 +0000

    fix(dotenv): read with tokenFIle

--- src/lib/fs.js
+++ src/lib/fs.js
@@ -1,8 +1,7 @@
 import fs from 'fs';
 import { promisify } from 'util';
 
-export const readFile = async (filePath, option = { encoding: 'utf-8' }) => {
-  await promisify(fs.readFile)(filePath, option);
-};
+export const readFile = (filePath, option = { encoding: 'utf-8' }) =>
+  promisify(fs.readFile)(filePath, option);
 export const writeFile = promisify(fs.writeFile);
 export const mkdir = promisify(fs.mkdir);
